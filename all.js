b696f2832439de0dbb7639da7cbfb3291211720a
jianshiangsanho <125791111+jianshiangsanho@users.noreply.github.com>
2023-03-27 15:04:24 +0800

    Update all.js

--- all.js
+++ all.js
@@ -30,26 +30,33 @@ function loadTasks() {
 function addTask() {
   const task = document.querySelector("form input");
   const list = document.querySelector("ul");
+
   // return if task is empty
   if (task.value === "") {
     alert("Please add some task!");
     return false;
   }
-  // check is task already exist
-  /*if (document.querySelector(`input[value="${task.value}"]`)) {
-    alert("Task already exist!");
-    return false;
-  }*/
+
+  // get current date in month/day format
+  const today = new Date();
+  const date = `${today.getMonth() + 1}/${today.getDate()}`;
 
   // add task to local storage
-  localStorage.setItem("tasks", JSON.stringify([...JSON.parse(localStorage.getItem("tasks") || "[]"), { task: task.value, completed: false }]));
+  localStorage.setItem(
+    "tasks",
+    JSON.stringify([
+      ...JSON.parse(localStorage.getItem("tasks") || "[]"),
+      { task: `${date} - ${task.value}`, completed: false },
+    ])
+  );
 
   // create list item, add innerHTML and append to ul
   const li = document.createElement("li");
   li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check">
-  <input type="text" value="${task.value}" class="task" onfocus="getCurrentTask(this)" onblur="editTask(this)">
-  <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
+    <input type="text" value="${date} - ${task.value}" class="task" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+    <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
   list.insertBefore(li, list.children[0]);
+
   // clear input
   task.value = "";
 }
